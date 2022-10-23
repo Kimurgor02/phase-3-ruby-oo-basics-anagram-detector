26dd83d2cf48a12806357cc531cdcd94a87f237a
Allan <allan.kimurgor@student.moringaschool.com>
2022-10-23 15:16:05 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match(array_of_words)
+        array_of_words.select do |w|
+            w.split("").sort == @word.split("").sort
+            
+        end
+    end
+end
